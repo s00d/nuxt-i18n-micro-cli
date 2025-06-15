1b9f28304b387c0cad36861998c6c1fd93d96558
Pavel Kuzmin <Virus191288@gmail.com>
2025-06-15 22:58:58 +0500

    feat(commands): add new commands for format, split, backup, and restore

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,6 +3,8 @@ import type { CommandDef } from 'citty'
 const _rDefault = (r: any) => (r.default || r) as Promise<CommandDef>
 
 export const commands = {
+  'format': () => import('./format').then(_rDefault),
+  'split': () => import('./split').then(_rDefault),
   'import': () => import('./import').then(_rDefault),
   'export': () => import('./export').then(_rDefault),
   'extract': () => import('./extract').then(_rDefault),
@@ -17,4 +19,6 @@ export const commands = {
   'check-duplicates': () => import('./check-duplicates').then(_rDefault),
   'replace-values': () => import('./replace-values').then(_rDefault),
   'text-to-i18n': () => import('./text-to-i18n').then(_rDefault),
+  'backup': () => import('./backup').then(_rDefault),
+  'restore': () => import('./restore').then(_rDefault),
 } as const
